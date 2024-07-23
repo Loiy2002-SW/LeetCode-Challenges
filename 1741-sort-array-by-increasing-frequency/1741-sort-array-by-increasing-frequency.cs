2cdbc6e0565ea66f1835dcdaaf6adb720fe0b65e
Loai Hasan <lloiy9310@gmail.com>
2024-07-23 09:03:07 +0300

    Time: 115 ms (40%), Space: 48.7 MB (8.75%) - LeetHub

---
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int[] FrequencySort(int[] nums) {
+         return nums.GroupBy(x => x)
+                   .OrderBy(g => g.Count()) 
+                   .ThenByDescending(g => g.Key) 
+                   .SelectMany(g => g)
+                   .ToArray();
+    }
+}
