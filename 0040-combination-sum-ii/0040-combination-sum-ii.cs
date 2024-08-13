c2478dd9a60fa6855f0ad7fc3f87730bc4d5d3f7
Loai Hasan <lloiy9310@gmail.com>
2024-08-13 10:41:06 +0300

    Time: 98 ms (91.23%), Space: 46.4 MB (70.96%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cs
@@ -0,0 +1,34 @@
+public class Solution
+{
+    List<int> partialRes = new List<int>();
+    List<int> sortedCandidates;
+    IList<IList<int>> res;
+	
+    public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+    {
+        res = new List<IList<int>>();
+        sortedCandidates = candidates.OrderBy(t => t).ToList();
+        BackTrack(0, target);
+        return res;
+    }
+	
+    private void BackTrack(int currentCandidate, int currentTarget)
+    {
+        var prevC = -1;
+        for (int i = currentCandidate; i < sortedCandidates.Count; i++)
+        {
+            var c = sortedCandidates[i];
+            if (c > currentTarget)
+                return;
+            if (prevC == c)
+                continue; 
+            partialRes.Add(c);
+            if (c == currentTarget)
+                res.Add(partialRes.ToArray());
+            else
+                BackTrack(i + 1, currentTarget - c);
+            partialRes.Remove(c);
+            prevC = c;
+        }
+    }
+}
