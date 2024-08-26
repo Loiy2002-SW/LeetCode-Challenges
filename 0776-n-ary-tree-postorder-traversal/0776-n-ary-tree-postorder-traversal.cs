5bd5d6dd843e813ad264398db78c31a8acef691a
Loai Hasan <lloiy9310@gmail.com>
2024-08-26 13:19:03 +0300

    Time: 147 ms (30.6%), Space: 57.3 MB (5.22%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.cs
@@ -0,0 +1,25 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, IList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+public class Solution {
+    public IList<int> Postorder(Node root) => root switch {
+        null => [],
+        _ => [..root.children.SelectMany(Postorder), root.val]
+    };
+}
